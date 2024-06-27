2497d748cf9a7f8cb90cfbfaa5ed814c28305acc
Rares <6453351+raress96@users.noreply.github.com>
2024-06-27 19:42:22 +0300

    Display base58 private key.

--- course01/load-keypair.ts
+++ course01/load-keypair.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import bs58 from 'bs58';
 
 const readKeypair = getKeypairFromEnvironment("SECRET_KEY");
+
+const bs58SecretKey = bs58.encode(readKeypair.secretKey);
 
 console.log(
   `✅ Finished! We've loaded our keypair securely, using an env file! Our public key is: ${readKeypair.publicKey.toBase58()}`
 );
+
+console.log(`Private key base58 ${bs58SecretKey}`);

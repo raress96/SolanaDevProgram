90521db08ba840aa8732c73ea17eadcfef9fa7b1
Rares <6453351+raress96@users.noreply.github.com>
2024-07-26 12:34:29 +0300

    Course 08 escrow lab.

---
+++ escrow/programs/escrow/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+pub mod initialize;
+pub mod make_offer;
+pub mod take_offer;
+
+pub use initialize::*;
+pub use make_offer::*;
+pub use take_offer::*;
---
+++ escrow/programs/escrow/src/lib.rs
@@ -0,0 +1,36 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5CYtVSiW8aqygrMrw34mmXSdcA7Rt86V1MKGpVGSTjs9");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        handler(ctx)
+    }
+
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        save_offer(context, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        send_wanted_tokens_to_maker(&context)?;
+        withdraw_and_close_vault(context)
+    }
+}

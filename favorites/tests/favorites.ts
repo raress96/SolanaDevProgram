93231faee4ecba08b5797cc5c6a39bd9fe3ef804
Rares <6453351+raress96@users.noreply.github.com>
2024-07-26 09:44:42 +0300

    Course 08 favorites lab.

--- favorites/tests/favorites.ts
+++ favorites/tests/favorites.ts
@@ -1,12 +1,62 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Favorites } from "../target/types/favorites";
+import { assert } from "chai";
+const web3 = anchor.web3;
 
 describe("favorites", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const user = (provider.wallet as anchor.Wallet).payer;
 
   const program = anchor.workspace.Favorites as Program<Favorites>;
+
+  before(async () => {
+    const balance = await provider.connection.getBalance(user.publicKey);
+    const balanceInSOL = balance / web3.LAMPORTS_PER_SOL;
+    const formattedBalance = new Intl.NumberFormat().format(balanceInSOL);
+    console.log(`Balance: ${formattedBalance} SOL`);
+  });
+
+  it("Saves a user's favorites to the blockchain", async () => {
+    // Here's what we want to write to the blockchain
+    const favoriteNumber = new anchor.BN(23);
+    const favoriteColor = "purple";
+    const favoriteHobbies = ["skiing", "skydiving", "biking"];
+
+    await program.methods
+      .setFavorites(favoriteNumber, favoriteColor, favoriteHobbies)
+      .signers([user])
+      .rpc();
+
+    // Now check everything matches
+    const favoritesPdaAndBump = web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("favorites"), user.publicKey.toBuffer()],
+      program.programId
+    );
+    const favoritesPda = favoritesPdaAndBump[0];
+
+    const dataFromPda = await program.account.favorites.fetch(favoritesPda);
+
+    assert.equal(dataFromPda.color, favoriteColor);
+    assert.equal(dataFromPda.number.toString(), favoriteNumber.toString());
+    assert.deepEqual(dataFromPda.hobbies, favoriteHobbies);
+  });
+
+  it("Doesn't let people write to favorites for other users", async () => {
+    const someRandomGuy = anchor.web3.Keypair.generate();
+
+    try {
+      await program.methods
+        .setFavorites(new anchor.BN(420), "red", ["being a dork"])
+        .signers([someRandomGuy])
+        .rpc();
+    } catch (error) {
+      const errorMessage = (error as Error).message;
+      assert.isTrue(errorMessage.includes("unknown signer"));
+    }
+  });
 
   it("Is initialized!", async () => {
     // Add your test here.
